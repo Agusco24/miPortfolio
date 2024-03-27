dc0f4b458a875724e6774ebe03f3e6d5cd02cbd4
Agusco24 <aguscolongne@gmail.com>
2024-03-27 17:37:26 -0300

    segundo commit

--- port.py
+++ port.py
@@ -1,31 +1,35 @@
 from flask import Flask, render_template, redirect, url_for, request
 from flask_mail import Mail, Message
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
+    
+    app.config.from_mapping(
+        DEBUG = False,
+        SECRET_KEY = "devTodoList",
+        
+    )
+
+    @app.route("/")
+    def index():
+        return render_template("index.html")
 
 
 
-mail = Mail(app)
+    @app.route("/cv")
+    def cv():
+        return render_template("cv.html")
 
+    @app.route("/todoList")
+    def todoList():
+        return render_template("todoList.html")
 
-@app.route("/")
-def index():
-    return render_template("index.html")
+    @app.route("/blogPost")
+    def blogPost():
+        return render_template("blogPost.html")
 
-
-
-@app.route("/cv")
-def cv():
-    return render_template("cv.html")
-
-@app.route("/todoList")
-def todoList():
-    return render_template("todoList.html")
-
-@app.route("/blogPost")
-def blogPost():
-    return render_template("blogPost.html")
-
-@app.route("/apiRest")
-def apiRest():
-    return render_template("apiRest.html")
+    @app.route("/apiRest")
+    def apiRest():
+        return render_template("apiRest.html")
+    
+    return app
